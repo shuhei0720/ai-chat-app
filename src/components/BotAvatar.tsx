cf27112ba8110924a437be1d7f056223a773f5a1
shuhei0720 <shuhei_0720@icloud.com>
2025-02-23 03:33:46 +0900

    'サイドバーのデザイン作成'

--- src/components/BotAvatar.tsx
+++ src/components/BotAvatar.tsx
@@ -4,7 +4,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 const BotAvatar = () => {
   return (
-    <Avatar>
+    <Avatar className="h-8 w-8">
         <AvatarImage src="/ai_logo.svg" />
         <AvatarFallback>CN</AvatarFallback>
     </Avatar>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,11 +3,13 @@ import BotAvatar from './BotAvatar'
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className='bg-gray-900 text-white p-3 h-full'>
         {/* タイトル&ロゴエリア */}
         <div className='flex items-center'>
-            <BotAvatar />
-            <h1>AI Chat App</h1>
+            <div className='mr-3'>
+                <BotAvatar />
+            </div>
+            <h1 className='font-bole text-xl'>AI Chat App</h1>
         </div>
 
         {/* チャットタイプエリア */}
