177d18748b1c7da29a28373f2425adc82ecad691
shuhei0720 <shuhei_0720@icloud.com>
2025-02-23 02:47:56 +0900

    '高さを100%に調整'

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,7 +4,7 @@ export default function DashboardRayout({
     children: React.ReactNode;
   }>) {
     return (
-      <div className="flex">
+      <div className="flex h-full">
         <div className="hidden bg-red-400 lg:w-1/4 lg:block">Sidebar</div>
         <main className="bg-blue-400 w-full lg:w-3/4">{children}</main>
       </div>
