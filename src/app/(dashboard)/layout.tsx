e99108f6f63c61536c17208da6c00eb9d5723162
shuhei0720 <shuhei_0720@icloud.com>
2025-02-23 03:22:38 +0900

    'shadcnをインストールし、タイトルのひな形を作成'

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function DashboardRayout({
     children,
   }: Readonly<{
@@ -5,7 +7,7 @@ export default function DashboardRayout({
   }>) {
     return (
       <div className="flex h-full">
-        <div className="hidden bg-red-400 lg:w-1/4 lg:block">Sidebar</div>
+        <div className="hidden bg-red-400 lg:w-1/4 lg:block"><Sidebar /></div>
         <main className="bg-blue-400 w-full lg:w-3/4">{children}</main>
       </div>
     );
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>
+        {/* タイトル&ロゴエリア */}
+        <div>タイトル&ロゴエリア</div>
+    </div>
+  )
+}
+
+export default Sidebar
