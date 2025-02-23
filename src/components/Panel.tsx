d0f4906858920890771d12a09598e31b20047b57
shuhei0720 <shuhei_0720@icloud.com>
2025-02-23 23:03:46 +0900

    'チャットタイプパネルのデザインを作成'

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -2,17 +2,49 @@ import Image from 'next/image'
 import React from 'react'
 
 const Panel = ({chatType}: {chatType: string}) => {
-    console.log(chatType);    
+    console.log(chatType);
+
+    const getChatConfig = () => {
+      let imageUrl = "";
+      let message = "";
+
+      switch(chatType) {
+        case "conversation":
+          imageUrl = "/conversation_panel.svg";
+          message = "会話を始めよう";
+        break;
+        case "image_generation":
+          imageUrl = "/image_generation_panel.svg";
+          message = "画像を生成しよう";
+        break;
+        case "text_to_speech":
+          imageUrl = "/text_to_speech_panel.svg";
+          message = "テキストを音声に変換しよう";
+        break;
+        case "speech_to_text":
+          imageUrl = "/speech_to_text_panel.svg";
+          message = "音声をテキストに変換しよう";
+        break;
+        case "image_analysis":
+          imageUrl = "/image_analysis_panel.svg";
+          message = "画像を解析しよう";
+        break;
+      }
+
+      return {imageUrl, message}
+    }
+
+    const {imageUrl,message} = getChatConfig();
   return (
     <div className="h-full flex flex-col items-center justify-center">
         <div className="relative h-72 w-72">
         <Image
             alt="Empty" 
             fill 
-            // src={imageUrl} 
+            src={imageUrl} 
         />
         </div>
-        <p className="text-muted-foreground text-sm text-center">{"message"}</p>
+        <p className="text-muted-foreground text-sm text-center">{message}</p>
     </div>
   )
 }
