1a7361f2d19854f1d7687b4f5d79c943640e9109
shuhei0720 <shuhei_0720@icloud.com>
2025-02-23 21:35:42 +0900

    'メッセージルームのデザインを作成'

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -11,7 +11,7 @@ export default function DashboardRayout({
       <div className="hidden bg-red-400 lg:w-1/4 lg:block">
         <Sidebar />
       </div>
-      <main className="bg-blue-400 w-full lg:w-3/4">
+      <main className="bg-blue-300 w-full lg:w-3/4">
         <div className="flex flex-col h-full">
           <Navbar />
           {children}
--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import BotAvatar from '@/components/BotAvatar'
 
 const ChatMessage = () => {
   return (
-    <div>ChatMessage</div>
+    <div className='flex-1 p-4 space-y-4 overflow-auto'>
+      <div className='flex space-x-4'>
+        <BotAvatar />
+        <div>
+          {/* メッセージのタイプによってタグを変える */}
+          <div className='bg-white p-4 rounded-lg shadow break-all whitespace-pre-wrap'>
+            <p>message</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
