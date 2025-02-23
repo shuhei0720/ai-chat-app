18acd1a397003f6fb257c270fb7cdc58f7fdf156
shuhei0720 <shuhei_0720@icloud.com>
2025-02-23 14:16:09 +0900

    'contextAPIのエラーを解消'

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -3,10 +3,14 @@ import { auth } from "@/lib/firebase/firebaseClient";
 import { UserInfo } from "firebase/auth";
 import { createContext, ReactNode, useEffect, useState } from "react";
 
-const AuthContext = createContext(undefined);
+interface AuthContextState {
+  currentUser: UserInfo | null
+}
+const AuthContext = createContext<AuthContextState | undefined>(undefined);
 
 export const AuthContextProvider = ({children}: {children: ReactNode}) => {
 const[currentUser,setCurrentUser] = useState<UserInfo | null>(null);
+const[isLoading,setIsLoading] = useState(true);
 
 useEffect(() => {
   const unsubscribe = auth.onIdTokenChanged((user) => {
@@ -15,13 +19,14 @@ useEffect(() => {
     } else {
       setCurrentUser(null);
     }
+    setIsLoading(false);
   })
   return () => unsubscribe();
 },[]);
 
   return (
-    <AuthContext.Provider value={currentUser}>
-      {children}
+    <AuthContext.Provider value={{currentUser}}>
+      {!isLoading && children}
     </AuthContext.Provider>
   )
 }
