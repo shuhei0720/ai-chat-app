559c467decca35db407240093dcd5c4fc153b69d
shuhei0720 <shuhei_0720@icloud.com>
2025-02-23 14:09:24 +0900

    'contextAPI内でユーザー情報を取得する処理を作成'

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,10 +1,26 @@
-import { createContext, ReactNode } from "react";
+"use client"
+import { auth } from "@/lib/firebase/firebaseClient";
+import { UserInfo } from "firebase/auth";
+import { createContext, ReactNode, useEffect, useState } from "react";
 
 const AuthContext = createContext(undefined);
 
 export const AuthContextProvider = ({children}: {children: ReactNode}) => {
+const[currentUser,setCurrentUser] = useState<UserInfo | null>(null);
+
+useEffect(() => {
+  const unsubscribe = auth.onIdTokenChanged((user) => {
+    if(user) {
+      setCurrentUser(user);
+    } else {
+      setCurrentUser(null);
+    }
+  })
+  return () => unsubscribe();
+},[]);
+
   return (
-    <AuthContext.Provider value={"user"}>
+    <AuthContext.Provider value={currentUser}>
       {children}
     </AuthContext.Provider>
   )
