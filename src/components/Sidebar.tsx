e3871e8f51aa7b2f0c58b79683079c0a7dc1a1fd
shuhei0720 <shuhei_0720@icloud.com>
2025-02-23 12:00:05 +0900

    'サイドバ-用の配列を作成'

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,41 @@
 import React from 'react'
 import BotAvatar from './BotAvatar'
+import { FileImage, FileOutput, FileSearch2, MessageCircle, Speech } from 'lucide-react'
 
 const Sidebar = () => {
+
+  const routes = [
+    {
+        label: "Conversation",
+        href: "/conversation",
+        color: "text-violet-500",
+        Icon: MessageCircle
+    },
+    {
+        label: "Image Generation",
+        href: "/image_generation",
+        color: "text-blue-500",
+        Icon: FileImage
+    },
+    {
+        label: "Text To Speech",
+        href: "/text_to_speech",
+        color: "text-red-500",
+        Icon: FileOutput
+    },
+    {
+        label: "Speech To Text",
+        href: "/speech_to_text",
+        color: "text-green-500",
+        Icon: Speech
+    },
+    {
+        label: "Image Analysis",
+        href: "/image_analysis",
+        color: "text-orange-500",
+        Icon: FileSearch2
+    }
+  ]
   return (
     <div className='bg-gray-900 text-white p-3 h-full'>
         {/* タイトル&ロゴエリア */}
