248c75eeb86e27de4b77a218fe655051014b84fe
shuhei0720 <shuhei_0720@icloud.com>
2025-02-24 19:28:36 +0900

    'firestoreとstoregeの初期設定'

--- src/lib/firebase/firebaseAdmin.ts
+++ src/lib/firebase/firebaseAdmin.ts
@@ -1,10 +1,18 @@
 import { applicationDefault, initializeApp } from "firebase-admin/app";
-import { getApp, getApps } from "firebase/app";
+import { getApp, getApps } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+import { getStorage } from 'firebase-admin/storage';
 
 if(!getApps().length) {
   initializeApp({
     credential: applicationDefault(),
+    storageBucket: 'ai-chat-app-46cb8.firebasestorage.app'
   });
 } else {
   getApp();
 }
+
+const db = getFirestore();
+const bucket = getStorage().bucket();
+
+export {db, bucket};
