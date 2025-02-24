ec89df083c997d6da424da0dcf7765b3fcf6f10f
shuhei0720 <shuhei_0720@icloud.com>
2025-02-24 13:05:13 +0900

    'フォームで送信する機能を追加'

--- src/components/ChatForm.tsx
+++ src/components/ChatForm.tsx
@@ -3,22 +3,30 @@ import React from 'react'
 import { Form, FormControl, FormField, FormItem, FormLabel } from '@/components/ui/form'
 import { Textarea } from '@/components/ui/textarea'
 import { useForm } from 'react-hook-form'
+import { Button } from '@/components/ui/button'
 
 const ChatForm = () => {
   const form = useForm()
+
+  const onSubmit = (values: any) => {
+    console.log(values);
+  }
   return (
     <Form {...form}>
-      <FormField
-        control={form.control}
-        name="prompt"
-        render={(field) => (
-          <FormItem>
-            <FormControl>
-              <Textarea {...field}/>
-            </FormControl>
-          </FormItem>
-        )}
-      />
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="prompt"
+          render={({field}) => (
+            <FormItem>
+              <FormControl>
+                <Textarea {...field}/>
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button>save</Button>
+      </form>
     </Form>
 
   )
