c6df038a11460020dce14fb8b0e251c03205c130
shuhei0720 <shuhei_0720@icloud.com>
2025-02-24 13:09:34 +0900

    'フォームのデザインの作成'

--- src/components/ChatForm.tsx
+++ src/components/ChatForm.tsx
@@ -4,6 +4,7 @@ import { Form, FormControl, FormField, FormItem, FormLabel } from '@/components/
 import { Textarea } from '@/components/ui/textarea'
 import { useForm } from 'react-hook-form'
 import { Button } from '@/components/ui/button'
+import { Send } from 'lucide-react'
 
 const ChatForm = () => {
   const form = useForm()
@@ -12,22 +13,28 @@ const ChatForm = () => {
     console.log(values);
   }
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
-        <FormField
-          control={form.control}
-          name="prompt"
-          render={({field}) => (
-            <FormItem>
-              <FormControl>
-                <Textarea {...field}/>
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        <Button>save</Button>
-      </form>
-    </Form>
+    <div className='bg-white p-3'>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className='flex items-center space-x-2'>
+            <FormField
+              control={form.control}
+              name="prompt"
+              render={({field}) => (
+                <FormItem className='w-full flex'>
+                  <FormControl>
+                    <Textarea {...field} className='bg-slate-200' rows={1}/>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <Button variant={"ghost"}>
+              <Send />
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
 
   )
 }
