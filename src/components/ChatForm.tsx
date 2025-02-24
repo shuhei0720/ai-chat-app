2843e48fdb01340c336455292a7f10204752f5f3
shuhei0720 <shuhei_0720@icloud.com>
2025-02-24 12:47:42 +0900

    'ReactHookFormが導入されたformwo

--- src/components/ChatForm.tsx
+++ src/components/ChatForm.tsx
@@ -1,8 +1,26 @@
+"use client"
 import React from 'react'
+import { Form, FormControl, FormField, FormItem, FormLabel } from '@/components/ui/form'
+import { Textarea } from '@/components/ui/textarea'
+import { useForm } from 'react-hook-form'
 
 const ChatForm = () => {
+  const form = useForm()
   return (
-    <div>ChatForm</div>
+    <Form {...form}>
+      <FormField
+        control={form.control}
+        name="prompt"
+        render={(field) => (
+          <FormItem>
+            <FormControl>
+              <Textarea {...field}/>
+            </FormControl>
+          </FormItem>
+        )}
+      />
+    </Form>
+
   )
 }
 
