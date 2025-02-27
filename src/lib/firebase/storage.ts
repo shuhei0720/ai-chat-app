63dfb0949f7f9e850634f12d44b87f46f4635ae1
shuhei0720 <shuhei_0720@icloud.com>
2025-02-27 23:53:11 +0900

    'ストレージの保存処理と参照URL発行処理を実装'

--- src/lib/firebase/storage.ts
+++ src/lib/firebase/storage.ts
@@ -1,13 +1,28 @@
 import { bucket } from "./firebaseAdmin";
 import { v4 as uuidv4 } from 'uuid';
 
-export const fileUploadToStorage = (
+export const fileUploadToStorage = async(
   buffer:Buffer,
   filePath: string,
   contentType: string,
 ) => {
-  const fileExtention = contentType === "audio/mpeg" ? "mp3" : contentType.split("/")[1];
-  const randomFileName = uuidv4();
-  const fileName = `${filePath}/${randomFileName}.${fileExtention}`;
-  const uploadFile = bucket.file(fileName);
+  try {
+    const fileExtention = contentType === "audio/mpeg" ? "mp3" : contentType.split("/")[1];
+    const randomFileName = uuidv4();
+    const fileName = `${filePath}/${randomFileName}.${fileExtention}`;
+    const uploadFile = bucket.file(fileName);
+
+    await uploadFile.save(buffer, {
+      metadata: {
+        contentType: contentType,
+      }
+    });
+
+    await uploadFile.makePublic();
+    return `https://storage.googoleapis.com/${bucket.name/fileName}`;
+  } catch (error: any) {
+    console.log(error);
+    throw new Error("ファイルのアップロードに失敗しました。", error.message);
+  }
+  
 };
