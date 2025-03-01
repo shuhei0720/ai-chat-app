7a00b4c5e4670db23fb7e548bcc0455a86a9b785
shuhei0720 <shuhei_0720@icloud.com>
2025-03-02 02:46:42 +0900

    'google認証方法を変更'

--- src/lib/firebase/firebaseAdmin.ts
+++ src/lib/firebase/firebaseAdmin.ts
@@ -1,11 +1,15 @@
-import { applicationDefault, initializeApp } from "firebase-admin/app";
+import { applicationDefault, cert, initializeApp } from "firebase-admin/app";
 import { getApp, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from 'firebase-admin/storage';
 
 if(!getApps().length) {
   initializeApp({
-    credential: applicationDefault(),
+    credential: cert({
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      project_id: process.env.FIREBASE_PROJECT_ID,
+    }),
     storageBucket: 'ai-chat-app-46cb8.firebasestorage.app'
   });
 } else {
