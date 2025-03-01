915201ee9b733099d79a395e5442cbf391c717fa
shuhei0720 <shuhei_0720@icloud.com>
2025-03-01 23:48:06 +0900

    'ユーザーが送信したテキストを表示'

--- src/components/ImageAnalysisMessageComponent.tsx
+++ src/components/ImageAnalysisMessageComponent.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Card } from './ui/card'
 import Image from 'next/image'
+import TextMessageComponent from './TextMessageComponent'
 
 const ImageAnalysisMessageComponent = ({content}: {content: { imageUrl: string[], text: string }}) => {
   return (
     <div>
       {content.imageUrl.length > 0 && (
       // 画像
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 mb-2">
         {content.imageUrl.map((url) => (
           <Card key={url} className="rounded-lg overflow-hidden">
             <div className="relative aspect-square">
@@ -23,6 +24,13 @@ const ImageAnalysisMessageComponent = ({content}: {content: { imageUrl: string[]
           </Card>
         ))}
       </div>
+      )}
+
+      {/* テキスト */}
+      {content.text && (
+        <div className='inline-block'>
+          <TextMessageComponent content={content.text}/>
+        </div>
       )}
     </div>
   )
