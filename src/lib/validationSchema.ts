08e97abdfc36d7d88a8fbb77f8b265c49790eaae
shuhei0720 <shuhei_0720@icloud.com>
2025-03-01 14:15:32 +0900

    '送信できるファイルの最大サイズをバリデーションスキーマでチェック'

--- src/lib/validationSchema.ts
+++ src/lib/validationSchema.ts
@@ -1,17 +1,34 @@
 import { z } from "zod";
 
+const MAX_AUDIO_FILE_SIZE = 1024 * 1024 * 20; //20MB
+
 export const conversationSchema = z.object({
   prompt: z.string().min(1,{message: "1文字以上入力してください。"}),
 })
 
 export const imageGenerationSchema = z.object({
-  prompt: z.string().min(1,{message: "1文字以上入力してください。"})
-  .max(1000,{message: "1000文字以下で入力してください。"}),
+  prompt: z
+    .string()
+    .min(1,{message: "1文字以上入力してください。"})
+    .max(1000,{message: "1000文字以下で入力してください。"}),
   amount: z.string(),
   size: z.string(),
 })
 
 export const textToSpeechSchema = z.object({
-  prompt: z.string().min(1,{message: "1文字以上入力してください。"})
-  .max(4096,{message: "4096文字以下で入力してください。"}),
+  prompt: z
+    .string()
+    .min(1,{message: "1文字以上入力してください。"})
+    .max(4096,{message: "4096文字以下で入力してください。"}),
+  })
+
+export const speechToTextSchema = z.object({
+  file: z
+    .instanceof(File, {message: "ファイルを選択してください。"})
+    //最大サイズ
+    .refine((file) => file.size <= MAX_AUDIO_FILE_SIZE, {
+      message: "20MB以下のファイルを選択してください。"
+    })
+    //ファイルの形式
+
 })
