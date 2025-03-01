e0dc4a5fe16fc1953bb74e53b27d81cab0836876
shuhei0720 <shuhei_0720@icloud.com>
2025-03-01 19:45:54 +0900

    'フロントから受け取ったデータをstorageとfirestoreに保存'

--- src/app/api/image_analysis/route.ts
+++ src/app/api/image_analysis/route.ts
@@ -11,21 +11,36 @@ const openai = new OpenAI({
 export async function POST(req: Request) {
   try {
     const formData = await req.formData();
-    const files = formData.getAll("files");
+    const files = formData.getAll("files") as File[];
     const prompt = formData.get("prompt");
-    const chatId = formData.get("chatId");
+    const chatId = formData.get("chatId") as string;
     console.log(files);
     console.log(prompt);
     console.log(chatId);
 
+    let urls:string[] = []
+
+    if(files.length > 0) {
+      //URL->ダウンロード->バイナリデータに変換->保存パスを設定->ストレージにアップロード
+      const imageDataPromises = files.map(async(file) => {
+        const arrayBuffer = await file.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+        const filePath = `${"er9CONFDWqNlIV6PnhGQbSM0ixl1"}/chatRoom/${chatId}`;
+        return await fileUploadToStorage(buffer, filePath, file.type);
+      });
+
+      urls = await Promise.all(imageDataPromises);
+      console.log("urls",urls);
+    }
+    
     // ユーザーメッセージをfirestoreに保存
     // Add a new document with a generated id.
-    // await db.collection("chats").doc(chatId).collection("messages").add({
-    //   content: prompt,
-    //   created_at: FieldValue.serverTimestamp(),
-    //   sender: "user",
-    //   type: "text",
-    // });
+    await db.collection("chats").doc(chatId).collection("messages").add({
+    content: {text:prompt, imageUrl: urls},
+      created_at: FieldValue.serverTimestamp(),
+      sender: "user",
+      type: "image_analysis",
+    });
 
     // openAI APIを呼び出してAIの回答を生成
     // const response = await openai.images.generate({
@@ -35,19 +50,7 @@ export async function POST(req: Request) {
     //   size: size,
     // });
 
-    //URL->ダウンロード->バイナリデータに変換->保存パスを設定->ストレージにアップロード
-    // const imageDataPromises = response.data.map(async(item) => {
-    //   if(item.url) {
-    //     const response = await fetch(item.url);
-    //     const arrayBuffer = await response.arrayBuffer();
-    //     const buffer = Buffer.from(arrayBuffer);
-    //     const filePath = `${"er9CONFDWqNlIV6PnhGQbSM0ixl1"}/chatRoom/${chatId}`;
-    //     return await fileUploadToStorage(buffer, filePath, "image/png");
-    //   }
-    // });
-
-    // const urls = await Promise.all(imageDataPromises);
-    // console.log("urls",urls);
+    
 
     // // AIの回答をfirestoreに保存
     // await db.collection("chats").doc(chatId).collection("messages").add({
