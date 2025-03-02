1f9d75b10d0a518dfec9aa8294c3442e3e68fd41
shuhei0720 <shuhei_0720@icloud.com>
2025-03-02 09:16:13 +0900

    'デプロイテスト'

--- src/lib/firebase/firebaseAdmin.ts
+++ src/lib/firebase/firebaseAdmin.ts
@@ -7,7 +7,7 @@ if(!getApps().length) {
   initializeApp({
     credential: cert({
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+      privateKey: (process.env.FIREBASE_PRIVATE_KEY || "").replace(/\\n/g, "\n"),
       projectId: process.env.FIREBASE_PROJECT_ID,
     }),
     storageBucket: 'ai-chat-app-46cb8.firebasestorage.app'
