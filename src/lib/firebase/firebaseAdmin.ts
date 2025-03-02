008ea5de8c22984d071e1cc398b93a79f472d7bf
shuhei0720 <shuhei_0720@icloud.com>
2025-03-02 09:10:56 +0900

    'デプロイエラーが出るのでAdminSDKの処理を元に戻す'

--- src/lib/firebase/firebaseAdmin.ts
+++ src/lib/firebase/firebaseAdmin.ts
@@ -3,17 +3,13 @@ import { getApp, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from 'firebase-admin/storage';
 
-const serviceAccountJsonData = process.env['MY_FIREBASE_SERVICE_ACCOUNT_JSON_DATA']
-const serviceAccountKey = JSON.parse(serviceAccountJsonData ?? "{}");
-
 if(!getApps().length) {
   initializeApp({
-    credential: cert(serviceAccountKey),
-    // credential: cert({
-    //   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    //   privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
-    //   projectId: process.env.FIREBASE_PROJECT_ID,
-    // }),
+    credential: cert({
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+      projectId: process.env.FIREBASE_PROJECT_ID,
+    }),
     storageBucket: 'ai-chat-app-46cb8.firebasestorage.app'
   });
 } else {
